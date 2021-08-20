c0f1aa54350f490d7d100a389bd652099df70b7f
SK2929 <i081zd@yahoo.co.jp>
2021-08-20 13:21:11 +0900

    エラー定数初期化
    productionモードで実行しようとした際に、エラーとなった。初期化せずに使用しようとするとエラーとなる

--- app/controllers/concerns/error_handlers.rb
+++ app/controllers/concerns/error_handlers.rb
@@ -3,8 +3,8 @@ module ErrorHandlers
 
   included do
     rescue_from StandardError, with: :rescue500
-    rescue_from Forbidden, with: :rescue403
-    rescue_from IpAddressRejected, with: :rescue403
+    rescue_from ApplicationController::Forbidden, with: :rescue403
+    rescue_from ApplicationController::IpAddressRejected, with: :rescue403
     rescue_from ActiveRecord::RecordNotFound, with: :rescue404
   end
 
