2b9e0746aa906369081de31b4fd20145020ade9b
SK2929 <i081zd@yahoo.co.jp>
2021-09-23 14:46:42 +0900

    ブロックホストの追記
    herokuで追記となったURLをブロックさせないため。

--- config/initializers/blocked_hosts.rb
+++ config/initializers/blocked_hosts.rb
@@ -2,4 +2,5 @@ Rails.application.configure do
   config.hosts << "example.com"
   config.hosts << "crms.example.com"
   config.hosts << "localhost"
+  config.hosts << "https://devcenter.heroku.com/articles/heroku-20-stack"
 end
