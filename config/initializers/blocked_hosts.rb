cea799f6872e1747e098cb1bfa5b8c064cbf206f
SK2929 <i081zd@yahoo.co.jp>
2021-09-23 15:46:40 +0900

    ブロックホスト修正
    再度修正

--- config/initializers/blocked_hosts.rb
+++ config/initializers/blocked_hosts.rb
@@ -2,5 +2,5 @@ Rails.application.configure do
   config.hosts << "example.com"
   config.hosts << "crms.example.com"
   config.hosts << "localhost"
-  config.hosts << "https://devcenter.heroku.com/articles/heroku-20-stack"
+  config.hosts << "fathomless-wave-25623.herokuapp.com"
 end
